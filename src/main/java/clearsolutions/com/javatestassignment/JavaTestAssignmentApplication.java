ce36c9500b0b3a845b2deff53daf53ba8da31562
Oleksandr Orlov <alexanderorlovkh@gmail.com>
2023-09-28 10:55:00 +0300

    Initial commit

---
+++ src/main/java/clearsolutions/com/javatestassignment/JavaTestAssignmentApplication.java
@@ -0,0 +1,13 @@
+package clearsolutions.com.javatestassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTestAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/clearsolutions/com/javatestassignment/JavaTestAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package clearsolutions.com.javatestassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
